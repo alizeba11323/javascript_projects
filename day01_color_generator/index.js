7508f0274f5e4336080dc8190ef2b7a818663316
alizeba11323 <alizeba11323@gmail.com>
2023-08-25 18:39:51 +0530

    added

--- day01_color_generator/index.js
+++ day01_color_generator/index.js
@@ -1,9 +1,10 @@
 const button = document.querySelector(".color_generate");
 const message = document.querySelector(".message");
 const color_container = document.querySelector("span.color");
-const copyColor = document.querySelector("span.copy_color");
+const copyColor = document.querySelector("button.copy_color");
 let color;
 function getColor() {
+  copyColor.removeAttribute("disabled");
   const rand = Math.floor(Math.random() * 0xffffff);
   color = `#${rand.toString(16)}`;
   color_container.innerText = color;
@@ -17,6 +18,13 @@ copyColor.addEventListener("click", function () {
   setTimeout(() => {
     message.innerText = "";
   }, 2000);
+  this.setAttribute("disabled", true);
 });
 
 getColor();
+
+// const btn = document.querySelector(".btn");
+// btn.addEventListener("click", function () {
+//   navigator.clipboard.writeText("hello");
+//   navigator.clipboard.readText("hello").then((data) => console.log(data));
+// });
