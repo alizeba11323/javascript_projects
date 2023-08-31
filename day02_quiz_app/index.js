d3540d8e617649ff8e8bd4dc633f4a54e9db0503
alizeba11323 <alizeba11323@gmail.com>
2023-08-31 16:52:17 +0530

    quiz functions

--- day02_quiz_app/index.js
+++ day02_quiz_app/index.js
@@ -1,11 +1,20 @@
 const quizContainer = document.querySelector(".quiz_container");
 const resultContainer = document.querySelector(".result_container");
 const welcomeContainer = document.querySelector(".welcome_container");
+const questionNo = document.querySelector(".question_no");
 const startBtn = document.querySelector(".start_btn");
 const question = document.querySelector(".question");
 const options = document.querySelectorAll(".option");
 const optionBtn = document.querySelector(".options");
 const nextBtn = document.querySelector(".next_btn");
+const result = document.querySelector(".result");
+const exit = document.querySelector(".exit");
+const replay = document.querySelector(".replay");
+const username = document.querySelector("#username");
+nextBtn.style.display = "none";
+resultContainer.style.display = "none";
+quizContainer.style.display = "none";
+startBtn.disabled = true;
 const questions = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
@@ -53,8 +62,10 @@ const questions = [
     ],
   },
 ];
-resultContainer.style.display = "none";
-quizContainer.style.display = "none";
+
+username.addEventListener("change", function () {
+  startBtn.disabled = false;
+});
 
 startBtn.addEventListener("click", function () {
   welcomeContainer.style.display = "none";
@@ -67,6 +78,7 @@ let total = questions.length;
 const loadQuestion = () => {
   const currentQuestion = questions[index];
   question.innerText = `${index + 1}) ${currentQuestion.question}`;
+  questionNo.innerText = `${index + 1} of ${total}`;
   Array.from(currentQuestion.answers).forEach((answer, idx) => {
     options[idx].innerText = answer.text;
     if (answer.correct) {
@@ -74,17 +86,41 @@ const loadQuestion = () => {
     }
   });
 };
+const resetButton = () => {
+  Array.from(options).forEach((btn) => {
+    if (btn.hasAttribute("correct")) {
+      btn.removeAttribute("correct");
+    }
+    btn.style.backgroundColor = "transparent";
+    btn.disabled = false;
+  });
+};
+const replayStart = () => {
+  index = 0;
+  score = 0;
+  resetButton();
+};
 
 nextBtn.addEventListener("click", function () {
-  console.log("clicked");
+  this.style.display = "none";
   if (index < total - 1) {
     index++;
+    if (index === 4) {
+      this.innerText = "Finish";
+    }
+    resetButton();
     loadQuestion();
+  } else {
+    quizContainer.style.display = "none";
+    result.innerText = `congrate ${username.value},Your score is ${score} out of ${total}`;
+    resultContainer.style.display = "block";
   }
 });
 optionBtn.addEventListener("click", function (e) {
   if (e.target.tagName === "BUTTON") {
+    nextBtn.style.display = "block";
     if (e.target.hasAttribute("correct")) {
+      score++;
       e.target.style.backgroundColor = "#1abc9c";
     } else {
       e.target.style.backgroundColor = "red";
@@ -94,5 +130,20 @@ optionBtn.addEventListener("click", function (e) {
         }
       });
     }
+    Array.from(options).forEach((btn) => {
+      btn.disabled = true;
+    });
   }
+});
+exit.addEventListener("click", function () {
+  resultContainer.style.display = "none";
+  welcomeContainer.style.display = "flex";
+  username.value = "";
+  replayStart();
+});
+replay.addEventListener("click", function () {
+  resultContainer.style.display = "none";
+  quizContainer.style.display = "block";
+  replayStart();
+  loadQuestion();
 });
