5f1fee7214b88b2b04109203a4d6ad2a56b44c0f
alizeba11323 <alizeba11323@gmail.com>
2023-09-15 16:51:52 +0530

    day04 finish

--- day04_form_validation/index.js
+++ day04_form_validation/index.js
@@ -1,12 +1,29 @@
-const fileUplaod = document.querySelector(".file-upload");
-const img = document.createElement("img");
-img.style.width = "100px";
-img.style.height = "100px";
-img.style.borderRadius = "50%";
-const image_container = document.querySelector(".image_container");
-fileUplaod.addEventListener("change", function (e) {
-  const target = e.target.files[0];
-  const url = URL.createObjectURL(target);
-  img.src = url;
-  image_container.prepend(img);
+const fileUpload = document.querySelector(".file-upload");
+const images = document.querySelector(".images");
+let imageArr = [];
+const loadImage = (imgArr) => {
+  Array.from(imgArr).forEach((image, idx) => {
+    const div = document.createElement("div");
+    div.className = "img_container";
+    const img = document.createElement("img");
+    img.className = "image";
+    img.src = URL.createObjectURL(image);
+    const span = document.createElement("span");
+    span.innerText = "x";
+    span.className = "close";
+    span.addEventListener("click", function (e) {
+      e.target.parentElement.remove();
+      imageArr = Array.from(imageArr).filter((i) => i !== image);
+      // imageArr.splice(idx, 1);
+      console.log(imageArr);
+    });
+    div.appendChild(img);
+    div.appendChild(span);
+    images.appendChild(div);
+  });
+};
+fileUpload.addEventListener("change", function (e) {
+  console.log(e.target.files);
+  imageArr = [...imageArr, ...Array.from(e.target.files)];
+  loadImage(e.target.files);
 });
